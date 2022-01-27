6eaedcad2a51198442d8ba19c512c9797d64c820
Mahdyar Hasanpour <git@mahdyar.me>
2022-01-27 18:47:05 +0330

    🎨 style: beutify the game

--- src/Game.js
+++ src/Game.js
@@ -69,17 +69,21 @@ class Game extends React.Component {
       status = "The game is Draw!";
     }
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board
-            squares={current.squares}
-            winningCombo={winner}
-            onClick={(i) => this.handleClick(i)}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <ol>{moves}</ol>
+      <div className="container">
+        <div className="game">
+          <div className="game-board">
+            <div className="squares-container">
+              <Board
+                squares={current.squares}
+                winningCombo={winner}
+                onClick={(i) => this.handleClick(i)}
+              />
+            </div>
+          </div>
+          <div className="game-info">
+            <div>{status}</div>
+            <ol>{moves}</ol>
+          </div>
         </div>
       </div>
     );
